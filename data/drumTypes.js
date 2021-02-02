1b3b2ac22637dc69262a8cc32a9314ca431230ca
ylu <ylu@ert.com>
2021-02-02 15:28:05 -0500

    init

---
+++ data/drumTypes.js
@@ -0,0 +1,26 @@
+export const drumTypes = [
+  {
+    type: 'kick',
+    sound: '/sounds/kick.mp3'
+  },
+  {
+    type: 'snare',
+    sound: '/sounds/snare.mp3'
+  },
+  {
+    type: 'hihat',
+    sound: '/sounds/hihat.mp3'
+  },
+  {
+    type: 'tom',
+    sound: '/sounds/tom.mp3'
+  },
+  {
+    type: 'cymbal',
+    sound: '/sounds/cymbal.mp3'
+  },
+  {
+    type: 'cowBell',
+    sound: '/sounds/cowbell.mp3'
+  }
+];
---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import Head from 'next/head'
+import Drum from '../components/Drum'
+import { drumTypes } from '../data/drumTypes'
+import styles from '../styles/Home.module.css'
+
+const Home: React.FC = () => {
+  const [drums] = useState(drumTypes)
+
+  const handlePlayDrum = (sound: string): void => {
+    const audio = new Audio(sound)
+    audio.play()
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next JS Drum Machine</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className={styles.main}>
+        {drums.map(drum => (
+          <Drum
+            key={drum.type}
+            drumType={drum.type}
+            onClick={() => handlePlayDrum(drum.sound)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
